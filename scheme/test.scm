05a96736f7cace8137aaaaeb45608f942295e6a6
tattsun <t.t.mc1192.sf@gmail.com>
2017-07-18 20:30:03 +0900

    scheme and rust

--- scheme/test.scm
+++ scheme/test.scm
@@ -1,1 +1,17 @@
-(+ 123 456)
+(define (square x) (* x x))
+
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+
+(define (f a)
+  (sum-of-squares (+ a 1) (* a 2)))
+
+
+(define (abs x)
+  (cond ((> x 0) x)
+        ((= x 0) 0)
+        ((< x 0) (- x))))
+
+(abs 1)
+(abs 0)
+
